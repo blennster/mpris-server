7e22fb956e110e56e9af8cedbeee50e94af29432
Axel Froborg <axel.froborg@gmail.com>
2022-09-16 16:36:21 +0200

    cleanup

---
+++ src/models/player.rs
@@ -0,0 +1,39 @@
+use mpris;
+
+const DEFAULT: &str = "Unknown";
+
+#[derive(serde::Serialize)]
+pub struct Player {
+    identity: String,
+    track: String,
+    artist: Vec<String>,
+    album_art: String,
+    volume: f64,
+    pos: Duration,
+    len: Duration,
+    can_play: bool,
+    can_go_next: bool,
+    can_go_prev: bool,
+}
+
+impl Player {
+    pub fn from_mpris(p: mpris::Player) -> Self {
+        let m = p.get_metadata().unwrap();
+
+        Self {
+            identity: p.identity().to_string(),
+            track: m.title().unwrap_or(DEFAULT).to_string(),
+            artist: m
+                .album_artists()
+                .unwrap_or(&vec![String::from(DEFAULT)])
+                .to_vec(),
+            album_art: m.art_url().unwrap().to_string(),
+            volume: p.get_volume().unwrap(),
+            pos: p.get_position().unwrap(),
+            len: m.length().unwrap(),
+            can_play: p.can_play().unwrap(),
+            can_go_next: p.can_go_next().unwrap(),
+            can_go_prev: p.can_go_previous().unwrap(),
+        }
+    }
+}
--- src/routes/mpris.rs
+++ src/routes/mpris.rs
@@ -3,39 +3,14 @@ use std::time::Duration;
 use actix_web::{get, HttpResponse, Responder};
 use mpris::PlayerFinder;
 
+use crate::models::player::Player;
+
 #[get("/")]
 pub async fn players() -> impl Responder {
-    let p = PlayerFinder::new().unwrap().find_active().unwrap();
+    let p = PlayerFinder::new().unwrap().find_active();
 
-    #[derive(serde::Serialize)]
-    struct Player {
-        identity: String,
-        track: String,
-        artist: Vec<String>,
-        volume: f64,
-        pos: Duration,
-        len: Duration,
-        can_play: bool,
-        can_go_next: bool,
-        can_go_prev: bool,
+    match p {
+        Some(p) => HttpResponse::Ok().json(Player::from_mpris(p)),
+        None => HttpResponse::NotFound().finish(),
     }
-
-    let m = p.get_metadata().unwrap();
-
-    let player = Player {
-        identity: p.identity().to_string(),
-        track: m.title().unwrap_or("Unknown").to_string(),
-        artist: m
-            .album_artists()
-            .unwrap_or(&vec![String::from("Unknown")])
-            .to_vec(),
-        volume: p.get_volume().unwrap(),
-        pos: p.get_position().unwrap(),
-        len: m.length().unwrap(),
-        can_play: p.can_play().unwrap(),
-        can_go_next: p.can_go_next().unwrap(),
-        can_go_prev: p.can_go_previous().unwrap(),
-    };
-
-    HttpResponse::Ok().json(player)
 }
